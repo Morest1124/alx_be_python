21d228b055886003dcaff0f0c50aaaf1e17a1a75
Morest1124 <morest1124burawa2005@gmail.com>
2025-07-12 00:20:31 +0200

    Make a sentences

--- basic_operations.py
+++ basic_operations.py
@@ -1,8 +1,8 @@
 number1 = 10;
 number2 = 5;
 result = number1 + number2
-print(result)
+print(f"Addition of {number1} and {number2} is {result}")
 result = number1 - number2
-print(result)
+print(f"Subtraction of {number1} and {number2} is {result}")
 result = number1 * number2
-print(result)
+print(f"Multiplication of {number1} and {number2} is {result}")
