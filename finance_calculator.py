bab8122a05944590aa57509038add3b29f12974f
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-07-12 15:20:05 +0200

    Add files via upload

--- finance_calculator.py
+++ finance_calculator.py
@@ -1,6 +1,7 @@
 monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = float(monthly_income) - float(monthly_expenses)
+
 print(f"Your monthly savings are ${monthly_savings}")
 
 # I = P(1 + IT**n)
