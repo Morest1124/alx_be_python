5d5753b634c6ed89cb0d3ed243d0255469de24e9
Morest1124 <morest1124burawa2005@gmail.com>
2025-08-03 21:58:54 +0200

    Add Data calculator

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+def get_current_datetime():
+    return datetime.now()
+
+def display_current_datetime():
+    current_date = get_current_datetime()
+    print(current_date.strftime("%d - %m - %Y %H : %M : %S"))
+
+def calculate_future_date():
+    current_date = get_current_datetime()
+    number_of_days = int(input('Enter how many days you want to add: '))
+    future_date = current_date + timedelta(days=number_of_days)
+    print(future_date.strftime("%d - %m - %Y %H : %M : %S"))
+
+display_current_datetime()
+calculate_future_date()
