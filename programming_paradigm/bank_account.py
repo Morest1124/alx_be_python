e2ba6eba7cf4e1b55a59926beb7ad05aeca69f9b
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-09 12:28:20 +0200

    Add files via upload

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+
+class BankAccount:
+
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        """Deposit the specified amount into the account"""
+        self.account_balance += amount
+        print(f"Deposited ${amount:.2f}. Current balance is ${self.account_balance:.2f}")
+
+    def withdraw(self, amount):
+        """Withdraw the specified amount from the account if funds are sufficient"""
+        if amount > self.account_balance:
+            print("Insufficient funds.")
+            return False
+        else:
+            self.account_balance -= amount
+            print(f"Withdrew ${amount:.2f}. Current balance is ${self.account_balance:.2f}")
+            return True
+
+    def display_balance(self):
+        """Display the current balance in a user-friendly format"""
+        print(f"Current balance: ${self.account_balance:.2f}")
+
