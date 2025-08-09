ae0e378b592e191a6f86aa033ae51b66bd503656
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-09 23:40:07 +0200

    Add files via upload

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.subtract(-1, 11), -12)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(11, 4), 44)
+        self.assertEqual(self.calc.divide(-1, 1), -1)
+        self.assertEqual(self.calc.divide(11, 11), 1)
+        self.assertEqual(self.calc.add(3, 3), 6)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.divide(-8, 1), -8)
+        # Add more assertions to thoroughly test the add method.
+
+# Remember to write additional test methods for subtract, multiply, and divide.
