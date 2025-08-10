2460b352ebaeb9f594fba25e035f18105b9ead1e
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-10 13:14:12 +0200

    Add a library management project

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,51 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def get_is_checked_out(self):
+        return self._is_checked_out
+
+    def set_is_checked_out(self, value):
+        self._is_checked_out = value
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title.lower() == title.lower() and not book._is_checked_out:
+                book._is_checked_out = True
+                print(f"You have checked out '{title}'.")
+                return
+            elif book.title.lower() == title.lower() and book._is_checked_out:
+                print(f"'{title}' is already checked out.")
+                return
+        print(f"'{title}' not found in library.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title.lower() == title.lower() and book._is_checked_out:
+                book._is_checked_out = False
+                print(f"You have returned '{title}'.")
+                return
+            elif book.title.lower() == title.lower() and not book._is_checked_out:
+                print(f"'{title}' is already available in library.")
+                return
+        print(f"'{title}' not found in library.")
+
+    def list_available_books(self):
+        available_books = [book.title for book in self._books if not book._is_checked_out]
+        if available_books:
+            print("Available books:")
+            for book in available_books:
+                print(book)
+        else:
+            print("No books are currently available.")
