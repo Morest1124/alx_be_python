6b2e2a2d3f2284ba7558cf02de59e30e0ed7a367
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-15 22:44:27 +0200

    Add files via upload

---
+++ oop/library_system.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title, author)-> None:
+        self.title = title
+        self.author = author
+        pass
+class Ebook:
+    
+    
+    
+    
+    
+    
+class PrintBook:
