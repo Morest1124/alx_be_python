7646494323c96c5918adc08af75c35f1b37449c1
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-16 12:27:05 +0200

    Update library_system.py, removed the type

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,23 +1,23 @@
 class Book:
-    def __init__(self, title, author) -> None:
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
 class EBook(Book):
-    def __init__(self, title, author, file_size) -> None:
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count) -> None:
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
 class Library:
-    def __init__(self) -> None:
+    def __init__(self):
         self.books = []
 
-    def add_book(self, book) -> None:
+    def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
@@ -28,3 +28,4 @@ class Library:
             elif isinstance(book, PrintBook):
                 print(f"Page Count: {book.page_count}")
             print()
+
