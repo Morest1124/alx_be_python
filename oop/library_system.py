21d0034a721fe73f14ec9e17e5580cb74863ddd9
Morest Burawa <98619022+Morest1124@users.noreply.github.com>
2025-08-16 11:59:35 +0200

    Add files via upload

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,13 +1,25 @@
+
 class Book:
     def __init__(self, title, author)-> None:
         self.title = title
         self.author = author
         pass
-class Ebook:
+class EBook(Book):
+    def __init__(self, file_size) -> None:
+        self.file_size  = file_size
+        pass
+
     
+class PrintBook(Book):
+        def __init__(self,  page_count):
+            self.page_count = page_count
+        pass
     
-    
-    
-    
-    
-class PrintBook:
+class Library:
+    def _add_book(self, Book, EBook, PrintBook) -> None:
+        self.Book = Book
+        self.EBook = EBook
+        self.PrintBook = PrintBook
+        pass
+    def list_book(self):
+        pass
